946bdc26764c5a3b0d185213ce38c0364745793e
Simon Sladek <sladeksi.work@gmail.com>
2025-07-23 14:17:38 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-white text-black">
+      <h1 className="text-4xl font-bold">LabGeneUse je na světě! 🚀</h1>
+    </main>
+  )
+}
